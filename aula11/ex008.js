6b8dbcbbe64ccba5ca4781d9caa6d3901efef9bd
luanfmrodrigues <luanrwork@gmail.com>
2024-05-20 15:38:41 +0100

    xx

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.8
+console.log(`a velocidade do seu carro é ${vel}`)
+if (vel > 60) { //condições simples
+    console.log(` Voçe ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre usando cinto de seguranca!`)
